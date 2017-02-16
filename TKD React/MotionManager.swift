d2bd3e437e918bdbfc515a484ce485ec359467e5
Bobby Ren <bobby_ren@comcast.com>
2017-02-16 13:58:08 -0500

    average motion changed to magnitude

--- TKD React/MotionManager.swift
+++ TKD React/MotionManager.swift
@@ -21,9 +21,9 @@ class MotionManager: NSObject {
             let absY = abs(y)
             let absZ = abs(z)
             
-            let average = Double((absX + absY + absZ) / 3)
+            let magnitude = Double(sqrt(absX*absX + absY*absY + absZ*absZ))
             
-            DataManager.sharedInstance.logMotion(average)
+            DataManager.sharedInstance.logMotion(magnitude)
         }
     }
 
